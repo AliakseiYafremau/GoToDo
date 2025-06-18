c9c5fc302e58fedd27ac2b1d490e180df32f4b06
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-06-18 20:23:29 +0200

    Add test for create task using sqlite repo

---
+++ repositories/sqlite/task_repo_test.go
@@ -0,0 +1,40 @@
+package sqlite_repositories_test
+
+import (
+	"testing"
+
+	"github.com/AliakseiYafremau/GoToDo/repositories/sqlite"
+)
+
+func setup_repo() (*sqlite_repositories.SqliteTaskRepository, error) {
+	db, err := sqlite_repositories.NewSqliteDB(":memory:")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return sqlite_repositories.NewSqliteTaskRepository(db), nil
+}
+
+func TestCreateTask(t *testing.T) {
+	repo, err := setup_repo()
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	task_text := "Test task"
+	task, err := repo.Create(task_text)
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if task.ID != 1 {
+		t.Errorf("unexpected id, expected 1, got %d", task.ID)
+	}
+
+	if task.Text != task_text {
+		t.Errorf("unexpected text, expected %s, got %s", task_text, task.Text)
+	}
+}
